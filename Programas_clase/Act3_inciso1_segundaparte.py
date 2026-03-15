6ab4232949c6019f9ae8894e7e16a7d3cfa4fccf
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-03-14 23:36:04 -0600

    Create Act3_inciso1_segundaparte.py script
    Add a script to validate a mathematical operation.

---
+++ Programas_clase/Act3_inciso1_segundaparte.py
@@ -0,0 +1,18 @@
+Autor: Daina Daiana Jimenez Olivera
+Autor: Hëctor Daniel Cervantes Cañedo
+Materia: Programación
+Grupo: 9157
+Tema: Actividad 3 inciso 1 del segundo apartado
+Fecha: 2026-02-12
+===========================================================
+
+OBJETIVO DEL SCRIPT
+-----------------------------------------------------------
+Programar el programa dado
+
+a = 1/947.0 * 947
+b = 1
+if a != b:
+    print("Wrong result!")
+else:
+    print("Right result!")
