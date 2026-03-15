843898eea06527b5443c6834b43e94b8dba62d78
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-03-14 23:22:23 -0600

    Add author Héctor Daniel Cervantes Cañedo

---
+++ Programas_clase/Act3_inciso9.java
@@ -0,0 +1,28 @@
+/**
+ * Programa corregido
+ *
+ * Curso: Programación
+ * Tema: Actividad 3
+ *
+ * Objetivo: Corregir el programo del inciso
+ *          9 con el mínimo de cambios
+ *
+ * Software: Java 25
+ *           IDE IntelliJ 2025.3.1
+ *
+ * @author Daina Daiana Jimenez Olivera
+ * @author Héctor Daniel Cervantes Cañedo
+ * Creado:  14-Mar-2026
+ */
+
+public class Correccion {
+    public static void main(String[] args) {
+        int a = 28;
+        double b = a;
+        System.out.printf("El valor es %.2f%n", b);
+        System.out.println("El valor de a es: " + a);
+        // Este es un comentario
+        /* 28 cabe en un tipo short */
+        short c = (short) a;
+    }
+}
