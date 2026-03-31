f4fff9a1505af33d778ae2ac11c6ed87ac18d672
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-03-31 16:30:32 -0600

    Add Act4_inciso1_segundaparte.java implementation

---
+++ Programas_clase/Act4_inciso1_segundaparte.java
@@ -0,0 +1,22 @@
+Autor: Daina Daiana Jimenez Olivera
+Autor: Hëctor Daniel Cervantes Cañedo
+Materia: Programación
+Grupo: 9157
+Tema: Actividad 4 inciso 1 del segundo apartado
+Fecha: 2026-03-31
+===========================================================
+
+OBJETIVO DEL SCRIPT
+-----------------------------------------------------------
+Programar el problema planteado en la actividad 4 
+en el inciso 1 del segundo apartado
+
+void main() {
+    int [] myIntArray = new int[3];
+    myIntArray[0] = 10;
+    myIntArray[2] = 20;
+    IO.println("Displaying the contents of the Array:");
+    for(int i = 0; i < myIntArray.length; i++){
+        System.out.print("\t" + myIntArray[i]);
+    }
+}
