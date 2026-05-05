c74ee786165d2fb615e730086fc95658d7d3e727
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-05-04 19:48:06 -0600

    Add Act5_inciso4_terceraparte.java with toString method

---
+++ Programas_clase/Act5_inciso4_terceraparte.java
@@ -0,0 +1,17 @@
+Autor: Daina Daiana Jimenez Olivera
+Autor: Hëctor Daniel Cervantes Cañedo
+Materia: Programación
+Grupo: 9157
+Tema: Actividad 5 inciso 4 del tercer apartado
+Fecha: 2026-05-4
+===========================================================
+
+OBJETIVO DEL SCRIPT
+-----------------------------------------------------------
+En las clases hijos sobrescribe el método toString().
+
+public String toString() {
+    return "Item [value=" + value + ", creator=" + creator + 
+           ", inventario=" + inventario + ", codigo=" + codigo + 
+           ", Condicion=" + Condicion + "]";
+}
