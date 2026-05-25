057af0f0ea96b82bd02294d91f73aca26f3f4318
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-05-24 20:56:32 -0600

    Create abstract Animal class with placeholder methods

---
+++ Programas_clase/claseabstracta/animal_abc.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    def hacer_sonido(self):
+        pass
+
+    def moverse(self):
+        pass
+
+    def nutricion(self):
+        pass
+
+    def sistemaEsqueletico(self):
+        pass
+
+    def reproduccion(self):
+        pass
