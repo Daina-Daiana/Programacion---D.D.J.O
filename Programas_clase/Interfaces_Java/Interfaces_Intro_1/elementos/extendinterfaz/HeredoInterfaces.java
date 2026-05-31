365b6dc0a0185cc4023b8488fa644a42c2ee5372
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-05-31 01:51:55 -0600

    Create HeredoInterfaces interface with default method

---
+++ Programas_clase/Interfaces_Java/Interfaces_Intro_1/elementos/extendinterfaz/HeredoInterfaces.java
@@ -0,0 +1,26 @@
+/**
+ * Curso: Programación
+ *
+ * Temas: Herencia múltiple de interfaces por otra iterfaz
+ *
+ * Software:
+ *      Java 8 y 25
+ *
+ * @author Roberto Méndez Méndez
+ * Editor Daina Daiana Jimenez Olivera
+ * Creación 20/ May/ 22
+ * Edición  31 Mayo 2026
+ */
+package elementos.extendinterfaz;
+
+import elementos.interfaz.ElementosBase1;
+import elementos.intrefazextra.IntrefazExtra;
+
+public interface HeredoInterfaces extends ElementosBase1, IntrefazExtra {
+
+    default void usoOtrasInterfaces(){
+        soyDefault();
+        System.out.println("pero también");
+        System.out.println(tuAsSommeil("j'ai sommeil"));
+    }
+}
