0d5815432540fd8f26d80e6bb38cc4b19aa24206
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-05-31 02:11:51 -0600

    Create SelectorGraficoDeArchivo2.java

---
+++ Programas_clase/Archivos/Proy_ManejoArchivos_intro_1_IntelliJ/selectordearchivo/SelectorGraficoDeArchivo2.java
@@ -0,0 +1,54 @@
+/**
+ * Programa que despliega un "Explorador de archivos" (Finder)
+ * para buscar y seleccionar un archivo
+
+ * Curso: Programación  2026-2
+
+ * Referencias:
+ * - https://docs.oracle.com/en/java/javase/25/docs/api/java.desktop/javax/swing/UIManager.html
+
+ * Software
+ *      Java 25
+ *      IDE IntelliJ 2026.1.2
+
+ * @author Roberto Méndez Méndez
+ * Editor Daina Daiana Jimenez Olivera
+ * Creación:  19 May 2026
+ * Edición:   31 May 2026
+ */
+package selectordearchivo;
+
+import javax.swing.JFileChooser;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import java.io.File;
+
+public class SelectorGraficoDeArchivo2 {
+
+    public static File seleccionaArchivo2() {
+        File archivo = null;
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException |
+                 IllegalAccessException | UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+
+        JFileChooser archivoSel = new JFileChooser();
+        int valor = archivoSel.showOpenDialog(archivoSel.getParent());
+        if (valor == JFileChooser.APPROVE_OPTION) {
+            try {
+                File tmpArch = archivoSel.getSelectedFile();
+                String ruta = tmpArch.getAbsolutePath();
+                archivo = new File(ruta);
+            } catch (SecurityException f) {
+                System.err.println("No se pudo accesar el archivo");
+            }
+        } else {
+            System.out.println("Al parecer no seleccionaste ningún archivo ");
+        }
+        return archivo;
+    }
+
+}
