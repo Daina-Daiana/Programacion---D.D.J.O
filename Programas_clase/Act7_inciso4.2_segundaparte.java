38843ed4ee904380b2dc92c31fe05d8dc737b588
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-05-31 12:02:00 -0600

    Create Act7_inciso4.2_segundaparte.java

---
+++ Programas_clase/Act7_inciso4.2_segundaparte.java
@@ -0,0 +1,10 @@
+import escribeaarchivo.EscribeAArchivoBasico;
+
+void main(String[] args) {
+    // Ya no se lanza RuntimeException, se maneja internamente con mensajes gráficos
+    EscribeAArchivoBasico.escribirAArchivoF1();
+
+    EscribeAArchivoBasico.escribirAArchivoF2();
+
+    EscribeAArchivoBasico.escribirAArchivoF3();
+}
