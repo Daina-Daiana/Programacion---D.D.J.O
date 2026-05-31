2db942952445357649da436ee342eb7686765f17
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-05-31 01:56:39 -0600

    Add InterfacesMultiples class implementing multiple interfaces

---
+++ Programas_clase/Interfaces_Java/Interfaces_Intro_1/elementos/InterfacesMultiples.java
@@ -0,0 +1,33 @@
+/**
+ * Curso: Programación
+ *
+ * Temas: Implementar varias Interfaces
+ *
+ * Software:
+ *      Java 8 y 25
+ *
+ * @author Roberto Méndez Méndez
+ * Editor Daina Daiana Jimenez Olivera
+ * Creación 20/ May/ 22
+ * Edición  31 Mayo 2026
+ */
+package elementos;
+
+import elementos.interfaz.ElementosBase1;
+import elementos.intrefazextra.IntrefazExtra;
+
+public class InterfacesMultiples implements ElementosBase1,
+        IntrefazExtra {
+
+    @Override // de ElementosBase1
+    public void debesImplementar(String pasameAlgo) {
+        System.out.println("c'est toi qui as écrit: " + pasameAlgo);
+    }
+
+    @Override  // de IntefazExtra
+    public String beMyFriend(boolean loveMe) {
+        if (loveMe)
+            return "Yes I'm your best fried";
+        return "Je te déteste";
+    }
+}
