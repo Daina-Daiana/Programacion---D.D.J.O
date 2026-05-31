55ff275b8b91b985c80017e0416032437849fb48
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-05-31 02:10:54 -0600

    Create SelectorGraficoDeArchivo1.java

---
+++ Programas_clase/Archivos/Proy_ManejoArchivos_intro_1_IntelliJ/selectordearchivo/SelectorGraficoDeArchivo1.java
@@ -0,0 +1,42 @@
+/**
+ * Programa que despliega un "Explorador de archivos" (Finder)
+ * para buscar y seleccionar un archivo
+
+ * Curso: Programación 2026-2
+
+ * Software
+ *      Java 25
+ *      IDE IntelliJ 2026.1.2
+ *
+ * @author Roberto Méndez Méndez
+ * Editor Daina Daiana Jimenez Olivera
+ * Creación:  16-Nov-21
+ * Edición:   31 May 2026
+ */
+package selectordearchivo;
+
+import javax.swing.JFileChooser;
+import java.io.File;
+
+public class SelectorGraficoDeArchivo1 {
+
+    public static File seleccionaArchivo1() {
+        File archivo = null;
+
+        JFileChooser archivoSel = new JFileChooser();
+        int valor = archivoSel.showOpenDialog(archivoSel.getParent());
+        if (valor == JFileChooser.APPROVE_OPTION) {
+            try {
+                File tmpArch = archivoSel.getSelectedFile();
+                String ruta = tmpArch.getAbsolutePath();
+                archivo = new File(ruta);
+            } catch (SecurityException f) {
+                System.err.println("No se pudo accesar el archivo");
+            }
+        } else {
+            System.out.println("Al parecer no seleccionaste ningún archivo ");
+        }
+        return archivo;
+    }
+
+}
