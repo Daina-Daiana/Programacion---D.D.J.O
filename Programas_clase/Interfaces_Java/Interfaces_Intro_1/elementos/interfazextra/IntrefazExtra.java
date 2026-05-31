78a0eeb7855c4a17555947a4bb0c6e4bf2c15f63
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-05-31 01:55:22 -0600

    Create IntrefazExtra interface with methods
    This interface provides a method for friendship and a default method to convert a string to a character array.

---
+++ Programas_clase/Interfaces_Java/Interfaces_Intro_1/elementos/interfazextra/IntrefazExtra.java
@@ -0,0 +1,32 @@
+/**
+ * Curso: Programación
+ *
+ * Uso: De apoyo "Herencia" múñtiple e
+ *      implementación múñtiple
+ *
+ * Temas: interface Elementos Básicos
+ *
+ * Extendida por:
+ *      HeredoInterfaces
+ *
+ * Software:
+ *      Java 8 y 25
+ *
+ * @author Roberto Méndez Méndez
+ * Editor Daina Daiana Jimenez Olivera
+ * Creación 20/ May/ 22
+ * Edición  31 Mayo 2026
+ */
+
+package elementos.intrefazextra;
+
+public interface IntrefazExtra {
+
+    String beMyFriend(boolean loveMe);
+
+    default char[] tuAsSommeil(String sommeil){
+        char[] sleep = sommeil.toCharArray();
+        return sleep;
+    }
+
+}
