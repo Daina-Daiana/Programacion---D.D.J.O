8cf7430bbaeb3c33ed23a597fea59030c44cfba0
Daina-Daiana <dainadaianajimenezolivera@ciencias.unam.mx>
2026-05-31 02:14:20 -0600

    Create PruebaLeerArchivo.java

---
+++ Programas_clase/Archivos/Proy_ManejoArchivos_intro_1_IntelliJ/PruebaLeerArchivo.java
@@ -0,0 +1,39 @@
+/**
+ * Programa que Lee los datos de un archivo de texto o csv
+
+ * Curso: Programación  2026-2
+
+ * Referencias:
+
+ * Software
+ *      Java 25
+ *      IDE IntelliJ 2026.1.2
+
+ * @author Roberto Méndez Méndez
+ * Editor Daina Daiana Jimenez Olivera
+ * Creación:  Nov 2021
+ * Edición:   31 May 2026
+ */
+import lecturaarchivo.*;
+
+import javax.swing.*;
+
+void main()  {
+
+    try {
+        LeerArchivoBasico_Tarea.leerArchivoF1();
+    }catch (NullPointerException | IOException e){
+        JOptionPane.showMessageDialog(
+                null,"No se ha seleccionado el archivo");
+    }
+
+    try {
+        LeerArchivoBasico_Tarea.leerArchivoF2();
+    }catch (NullPointerException  e){
+        JOptionPane.showMessageDialog(
+                null,"No se ha seleccionado el archivo");
+    }
+
+
+
+}
